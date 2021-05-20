a88c9e39fe11d2e7b957109eb5f816376e96be12
Dipti Kartikeya <Dipti.Kartikeya@atcltd.atc.org.nz>
2021-05-20 21:56:36 +1200

    Add project files.

---
+++ Account.cs
@@ -0,0 +1,22 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace XamExistingDB
+{
+    public class Account
+    {
+        [PrimaryKey]
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Mobile { get; set; }
+    }
+}
---
+++ MainActivity.cs
@@ -0,0 +1,76 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using System.Collections.Generic;
+using Android.Database.Sqlite;
+using Android.Database;
+using Android.Views;
+using Android.Content;
+
+namespace XamExistingDB
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        //https://www.c-sharpcorner.com/article/xamarin-android-working-with-existing-db/
+        DBHelper db;
+        SQLiteDatabase sqliteDB;
+        LinearLayout container;
+        Button btnGetData;
+        List<Account> lstUser = new List<Account>();
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+           
+            db = new DBHelper(this);
+            sqliteDB = db.WritableDatabase;
+            container = FindViewById<LinearLayout>(Resource.Id.container);
+            btnGetData = FindViewById<Button>(Resource.Id.btnLoadData);
+            btnGetData.Click += delegate
+            {
+                Data();
+            };
+        }
+        private void Data()
+        {
+            ICursor selectData = sqliteDB.RawQuery("select * from Account", new string[] { });
+            if (selectData.Count > 0)
+            {
+                selectData.MoveToFirst();
+                do
+                {
+                    Account user = new Account();
+                    user.Name = selectData.GetString(selectData.GetColumnIndex("Name"));
+                    user.Email = selectData.GetString(selectData.GetColumnIndex("Email"));
+                    user.Mobile = selectData.GetString(selectData.GetColumnIndex("Mobile"));
+                    lstUser.Add(user);
+                }
+                while (selectData.MoveToNext());
+                selectData.Close();
+            }
+            foreach (var item in lstUser)
+            {
+                LayoutInflater layoutInflater = (LayoutInflater)BaseContext.GetSystemService(Context.LayoutInflaterService);
+                View addView = layoutInflater.Inflate(Resource.Layout.row, null);
+                TextView txtName = addView.FindViewById<TextView>(Resource.Id.txtName);
+                TextView txtEmail = addView.FindViewById<TextView>(Resource.Id.txtEmail);
+                TextView txtMobile = addView.FindViewById<TextView>(Resource.Id.txtMobile);
+                txtName.Text = item.Name;
+                txtEmail.Text = item.Email;
+                txtMobile.Text = item.Mobile;
+                container.AddView(addView);
+            }
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
